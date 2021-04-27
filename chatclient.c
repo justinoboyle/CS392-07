3c3f8eaf6db1d00bbff82d366737a37d1588fe8c
Justin O'Boyle <justin@justinoboyle.com>
2021-04-27 19:57:10 -0400

    prompt for username

--- chatclient.c
+++ chatclient.c
@@ -20,6 +20,9 @@
 #define ERR_USAGE       "Usage: %s <server IP> <port>\n"
 #define ERR_INVALID_IP  "Error: Invalid IP address '%s'.\n"
 #define ERR_PORT_RANGE  "Error: Port must be in range [1024, 65535].\n"
+#define ERR_UNAME_LONG  "Sorry, limit your username to %d characters.\n"
+
+#define STR_USER_PROMPT "Enter your username: "
 
 #define PORT_RANGE_MIN  1024
 #define PORT_RANGE_MAX  65535
@@ -28,6 +31,8 @@
 // char username[MAX_NAME_LEN + 1];
 // char inbuf[BUFLEN + 1];
 // char outbuf[MAX_MSG_LEN + 1];
+
+int readUsername(char* uname);
 
 int handle_stdin() {
     return 1;
@@ -71,9 +76,53 @@ int main(int argc, char *argv[]) {
         fprintf(stderr, ERR_PORT_RANGE);
         return EXIT_FAILURE;
     }
+
+    char* uname = malloc((MAX_NAME_LEN + 2) * sizeof(char));
+
+    readUsername(uname);
+
+    printf("Hello, %s. Let's try to connect to the server.\n", uname);
+
+    free(uname);
+
     printf("Debug: %i | ", port);
     printf("Parse success\n");
+}
 
+int readUsername(char* uname) {
 
+    int ACTIVE = 1;
+    int BYTES_READ = -1;
 
+    while(ACTIVE) {
+        char tmp[BUFLEN];
+        fflush(stdin);
+        printf(STR_USER_PROMPT);
+        fflush(stdout);
+
+        if((BYTES_READ = read(STDIN_FILENO, tmp, BUFLEN)) == -1) {
+            fprintf(stderr, "%i Error: Could not read username.\n", BYTES_READ);
+            continue;
+        }
+
+        tmp[strcspn(tmp, "\n")] = 0;
+
+        if(BYTES_READ < 2) {
+            // EOF, flush line
+            if(BYTES_READ == 0)
+                printf("\n");
+            // re prompt
+            continue;
+        } else {
+            if((BYTES_READ-1) > MAX_NAME_LEN || strlen(tmp) > MAX_NAME_LEN) {
+                fprintf(stderr, ERR_UNAME_LONG, MAX_NAME_LEN);
+                ACTIVE = 1;
+                continue;
+            } else {
+                strcpy(uname, tmp);
+                ACTIVE = 0;
+            }
+        }
+    }
+    return 0;
 }
